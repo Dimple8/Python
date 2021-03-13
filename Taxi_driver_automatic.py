f01804e0283d3c0d0fc1b2782f8727e0fb7f0a52
Dmitrii <dimple395@gmail.com>
2021-03-14 00:32:18 +0300

    Added logging

--- Taxi_driver_automatic.py
+++ Taxi_driver_automatic.py
@@ -1,9 +1,14 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import os
+import logging
 try:
     from configparser import ConfigParser
 except ImportError:
     from ConfigParser import ConfigParser  # ver. < 3.0
+
+THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
+logging.basicConfig(filename=os.path.join(THIS_FOLDER, 'result.log'), encoding='utf-8', level=logging.INFO)
 
 def fill_element(elementName, text):
     elem = driver.find_element_by_name(elementName)
@@ -13,8 +18,9 @@ def fill_element(elementName, text):
 # instantiate
 config = ConfigParser()
 
-# parse existing file
-config.read('settings.ini')
+# read settings.ini file
+settings = os.path.join(THIS_FOLDER, 'settings.ini')
+config.read(settings)
 
 # read values from a settings.ini file
 baseUrl = config.get('DEFAULT', 'Url')
@@ -25,23 +31,15 @@ phone = config.get('DEFAULT', 'PhoneField')
 driver = webdriver.Safari()
 driver.get(baseUrl)
 
-print(driver.title)
-
 driver.switch_to.frame(driver.find_element_by_xpath('//iframe[starts-with(@src, "https://forms.yandex.ru/surveys/10017467/?iframe=1&theme=&lang=ru&click_id=")]'))
 
-fill_element(name, "Водитель_1")
-fill_element(phone, "8896464")
-
-# try:
-#     usernameElem = driver.find_element_by_name(nameElement)
-#     usernameElem.clear()
-#     usernameElem.send_keys("Driver1")
-
-#     phoneElem = driver.find_element_by_name(phoneElement)
-#     phoneElem.clear()
-#     phoneElem.send_keys("12345678")
-# except:
-#     print("Произошла ошибка при вводе данных элементу , 'ФИО'.")
+driverName = "Водитель_1"
+driverPhone = "8896464"
+# enter data
+fill_element(name, driverName)
+fill_element(phone, driverPhone)
+print('Обработаны данные для: ' + driverName + ' - ' + driverPhone)
+logging.info('Обработаны данные для: ' + driverName + ' - ' + driverPhone)
 
 driver.switch_to.default_content()
 driver.close()
