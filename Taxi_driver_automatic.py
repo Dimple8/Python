21dedf40b6cdee9a3397bf31e67c3eba7e2da4e6
Dmitrii <dimple395@gmail.com>
2021-03-15 00:32:48 +0300

    Added reading excel file

--- Taxi_driver_automatic.py
+++ Taxi_driver_automatic.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import os
+import pandas
+from collections import defaultdict
 import logging
 try:
     from configparser import ConfigParser
@@ -15,6 +17,13 @@ def fill_element(elementName, text):
     elem.clear()
     elem.send_keys(text)
 
+def get_list_from_excel_file(excelFile):
+    THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
+    filePath = os.path.join(THIS_FOLDER, excelFile)
+    excel_data_df = pandas.read_excel(filePath, usecols=['ФИО', 'Корректный телефон', 'Click ID'])
+    return excel_data_df.to_dict('record')
+
+
 # instantiate
 config = ConfigParser()
 
@@ -27,19 +36,25 @@ baseUrl = config.get('DEFAULT', 'Url')
 name = config.get('DEFAULT', 'NameField')
 phone = config.get('DEFAULT', 'PhoneField')
 
+# get items from excel file
+items = get_list_from_excel_file('Data.xlsx')
+
 # open browser
 driver = webdriver.Safari()
-driver.get(baseUrl)
 
-driver.switch_to.frame(driver.find_element_by_xpath('//iframe[starts-with(@src, "https://forms.yandex.ru/surveys/10017467/?iframe=1&theme=&lang=ru&click_id=")]'))
+for item in items:
+    driverName = item['ФИО']
+    driverPhone = item['Корректный телефон']
+    driverID = item['Click ID']
 
-driverName = "Водитель_1"
-driverPhone = "8896464"
-# enter data
-fill_element(name, driverName)
-fill_element(phone, driverPhone)
-print('Обработаны данные для: ' + driverName + ' - ' + driverPhone)
-logging.info('Обработаны данные для: ' + driverName + ' - ' + driverPhone)
+    driver.get(baseUrl + driverID)
+    driver.switch_to.frame(driver.find_element_by_xpath('//iframe[starts-with(@src, "https://forms.yandex.ru/surveys/10017467/?iframe=1&theme=&lang=ru&click_id=")]'))
 
-driver.switch_to.default_content()
+    # enter data
+    fill_element(name, driverName)
+    fill_element(phone, driverPhone)
+    # print('Обработаны данные для: ' + driverName + ' - ' + driverPhone)
+    # logging.info('Обработаны данные для: ' + driverName + ' - ' + driverPhone)
+
+    driver.switch_to.default_content()
 driver.close()
